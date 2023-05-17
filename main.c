774944d3912f18db8062f60485e7bc1311a124ed
yusfuldev <owokoniran93@gmail.com>
2023-05-17 15:57:07 -0700

    edited main.c and created prompt.c

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
+#include "shell.h"
+/**
+ * main- Entry point of shell.
+ * @argc: arguments count.
+ * @argv: arguments vector.
+ * Return: 0 otherwise negative integer.
+ */
+
 int main(int argc, char **argv)
 {
+	_prompt(argc, argv);
         return (0);
 }
