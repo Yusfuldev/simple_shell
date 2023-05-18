662108bfca5c3d7147023b1168f733b45b945a06
Patriziano <onahpatrick75@gmail.com>
2023-05-17 17:17:57 -0700

    header file

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+extern char **environ;
+void _prompt(int argc, char **argv);
+char *read_input();
+void signal_handler(int signum);
+
+#endif
