30dd2419d2ee7b163b3323a4690017de457a62d9
yusfuldev <owokoniran93@gmail.com>
2023-05-18 03:34:40 -0700

    customised strtok function

---
+++ strtok.c
@@ -0,0 +1,59 @@
+#include "shell.h"
+/**
+ * _strtok- breaks a string into a sequence of zero or more nonempty tokens.
+ * @str: the string to be parsed.
+ * @delim: set of bytes that delimit the tokens in the parsed string.
+ * Return: a pointer to a null-terminated string containing the next token.
+ */
+
+char *_strtok(char *str, char *delim)
+{
+        char *token = NULL, *delimpos = NULL;
+        static char *next_token = NULL;
+
+        if (str != NULL)
+                next_token = str;
+        if (!next_token || *next_token == '\0')
+                return (NULL);
+        token = next_token;
+        delimpos = _strstr(next_token, delim);
+        if (delimpos != NULL)
+        {
+                *delimpos = '\0';
+                next_token = delimpos + strlen(delim);
+        } else
+        {
+                next_token += strlen(next_token);
+        }
+        return (token);
+}
+
+/**
+ * _strstr- searches for string needle from haystack.
+ * @haystack: string to search from.
+ * @needle: string to search for.
+ *Return: pointer to the begining of the found string or NULL.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *n = needle;
+	char *h = haystack;
+	int i = 0, j = 0;
+
+	if (*n == '\0')
+		return (h);
+	while (h[i] != '\0')
+	{
+		j = 0;
+		while (n[j] != '\0')
+		{
+			if (h[i + j] != n[j])
+				break;
+			j++;
+		}
+		if (n[j] == '\0')
+			return ((char *)(h + i));
+		i++;
+	}
+	return (NULL);
+}
