f7c14098138217774fc3d12846d7df08382bbb7c
Patriziano <onahpatrick75@gmail.com>
2023-05-17 17:18:51 -0700

    function that reads command line

---
+++ input.c
@@ -0,0 +1,67 @@
+#include "shell.h"
+
+/**
+*read_input - get inputs from the user
+*
+Return: the buffer that contains user's input
+*/
+
+char *read_input()
+{
+	ssize_t read_char = 0;
+	size_t n = 0;
+	char *lineptr = NULL;
+
+	read_char = getline(&lineptr, &n, stdin)
+
+	if (read_char == -1)
+	{
+		free(lineptr);
+		if (isatty(STDIN_FILENO))
+		{
+			write(STDOUT_FILENO, "\n", 1);
+		}
+		exit(EXIT_FAILURE);
+	}
+
+	if (lineptr[read_char - 1] == '\n')
+	{
+		lineptr[read_char - 1] = '\0';
+	}
+
+	return (lineptr);
+}
+
+/**
+ *signal_handler - handles the SIGINT signal
+ *@signum: variables that handles the SIGINT
+ *
+ *Return: Nothing
+*/
+
+void signal_handler(int signum)
+{
+	if (signum == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+	}
+}
+
+/**
+ *free_args(char **args)
+ *@args: parsed arguments
+ *
+ *Return: Nothing
+*/
+
+void free_args(char **argv)
+{
+	int i = 0;
+
+	for ( i = 0; argv[i] != NULL; i++)
+	{
+		free(argv[i]);
+	}
+
+	free(argv);
+}
