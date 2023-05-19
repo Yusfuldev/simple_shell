4ef6eab4b17647a19b56dd004c6bde823fcbc09e
Patriziano <onahpatrick75@gmail.com>
2023-05-19 03:00:16 -0700

    implimenting exit with status

--- execute.c
+++ execute.c
@@ -8,7 +8,7 @@
 
 int execute(int ac, char **args)
 {
-	int status = 0;
+	int status = 0, exit_status = 0;
 /*	char *command = NULL;*/
 	pid_t child_pid;
 /*	char *message;*/
@@ -19,7 +19,17 @@ int execute(int ac, char **args)
 		return (1);
 	}*/
 	if (strcmp(args[0], "exit") == 0)
-		exit(0);
+	{
+		if (args[1] != NULL)
+		{
+			exit_status = atoi(args[1]);
+		}
+		else
+		{
+			exit_status = 0;
+		}
+		exit(exit_status);
+	}
 /*	command = path_handler(args);
 	if (command == NULL)
 	{
