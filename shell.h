322ec0686361454b448e78c3b57f404ffc59600e
Patriziano <onahpatrick75@gmail.com>
2023-05-26 04:50:21 -0700

    merging conflict

--- shell.h
+++ shell.h
@@ -43,7 +43,7 @@ char *read_input();
 void free_args(char **argv);
 void signal_handler(int signum);
 int process(char *command, char **args);
-int execute(int ac, char **args, char *path);
+int execute(int ac, char **args);
 char **tokenize(char *buffer, char *delim);
 char *_strtok(char *str, char *delim);
 char *_strstr(char *haystack, char *needle);
